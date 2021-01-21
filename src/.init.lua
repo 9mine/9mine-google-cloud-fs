445ac26447a133a740248d6f850432832cb5b09a
Oleksandr Dievri <o.dievri@gmail.com>
2021-01-21 16:43:37 +0200

    Update values to use captured path

--- src/.init.lua
+++ src/.init.lua
@@ -1,15 +1,15 @@
-local function set_texture(entry, entity, init_path)
-    local prefix = init_path == "/" and "" or init_path
-    if not prefix then return end
-    if entry.platform_path == prefix .. "/compute/instances/describe" then
-        texture.download(
-            "https://cloudacademy.com/wp-content/uploads/2014/04/ComputeEngine_512px-300x300.png",
-            true, "9mine-google-cloud-fs.png", "9mine-google-cloud-fs")
+local function set_texture(entry, entity)
+    local prefix = init_path:match("/$") and init_path:sub(1, -2) or init_path
+    if not prefix then
+        return
+    end
+    if entry.platform_string == prefix .. "/compute/instances/describe" then
+        texture.download("https://cloudacademy.com/wp-content/uploads/2014/04/ComputeEngine_512px-300x300.png", true,
+            "9mine-google-cloud-fs.png", "9mine-google-cloud-fs")
         entity:set_properties({
             visual = "cube",
-            textures = {"9mine-google-cloud-fs.png", "9mine-google-cloud-fs.png",
-                "9mine-google-cloud-fs.png", "9mine-google-cloud-fs.png",
-                "9mine-google-cloud-fs.png", "9mine-google-cloud-fs.png"}
+            textures = {"9mine-google-cloud-fs.png", "9mine-google-cloud-fs.png", "9mine-google-cloud-fs.png",
+                        "9mine-google-cloud-fs.png", "9mine-google-cloud-fs.png", "9mine-google-cloud-fs.png"}
         })
     end
 end
