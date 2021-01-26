52c7dd9320bd71e659c3a3bc5507289b363b6b64
Oleksandr Dievri <o.dievri@gmail.com>
2021-01-27 00:08:11 +0200

    fix pwdfields

--- formspec.lua
+++ formspec.lua
@@ -11,7 +11,7 @@ credentials_event = function(player, formname, fields)
             pcall(np_prot.file_write, conn.conn, "gcloud", content)
             minetest.show_formspec(player_name, "core:none",
                                    table.concat({"formspec_version[4]", "size[8,1,false]", "hypertext[0, 0.2; 8, 1;; <bigger><center>Credentials sent. Check registry<center><bigger>]"}, ""))
-            minetest.after(1, function() minetest.show_formspec(player_name, "core:none", "") end)
+            minetest.after(3, function() minetest.show_formspec(player_name, "core:none", "") end)
         end
     end
 end
@@ -21,5 +21,5 @@ register.add_form_handler("core:google_credentials", credentials_event)
 
 minetest.show_formspec(player_name, "core:google_credentials",
                        table.concat({"formspec_version[4]", "size[8,6,false]", "field[0,0;0,0;service_addr;;", minetest.formspec_escape(res), "]",
-    "hypertext[0, 0.3; 8, 1;; <bigger><center>Google Cloud<center><bigger>]", "pwdfield[0.5, 1.5; 7, 0.7;key;key (in base64);]", "pwdfield[0.5, 2.6; 7, 0.7;project;project name;]",
-    "pwdfield[0.5, 3.7; 7, 0.7;zone;zone;]", "button[5,4.8;2.5,0.7;send;send]"}, ""))
+    "hypertext[0, 0.3; 8, 1;; <bigger><center>Google Cloud<center><bigger>]", "pwdfield[0.5, 1.5; 7, 0.7;key;key (in base64)]", "pwdfield[0.5, 2.6; 7, 0.7;project;project name]",
+    "pwdfield[0.5, 3.7; 7, 0.7;zone;zone]", "button[5,4.8;2.5,0.7;send;send]"}, ""))
